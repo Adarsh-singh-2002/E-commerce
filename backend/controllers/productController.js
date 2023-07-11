c9cf7f3ac57d08e97451c72e8470237196e3b3d0
Adarsh <adarshriit2017@gmail.com>
2023-07-12 01:38:07 +0530

    12072023

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,6 +11,38 @@ exports.createProduct = async(req,res,next)=>{
     })
 }
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"Route is working fine"})
+//Get all products
+exports.getAllProducts = async(req,res)=>{
+    const products = await Product.find();
+    res.status(200).json({
+        success:true,
+        products
+    })
+}
+
+
+//update Product
+
+exports.updateProducts = async(req,res,next) => {
+
+    let product = Product.findById(req.params.id);
+
+    if(!product)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id.req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
 }
