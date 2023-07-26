cdb3c18234eb459308d2b379428c50d524bc802d
Adarsh <adarshriit2017@gmail.com>
2023-07-26 11:00:42 +0530

    26072023-userController completed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
-const { getAllProducts, createProduct, updateProducts, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { 
+    getAllProducts,
+     createProduct,
+     updateProducts,
+     deleteProduct,
+     getProductDetails,
+     } = require("../controllers/productController");
+
 const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
@@ -8,13 +15,14 @@ const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/products/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route("/admin/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
 router
-.route("/product/:id")
+.route("/admin/product/:id")
 .put(isAuthenticatedUser, authorizeRoles("admin"),updateProducts)
 .delete(isAuthenticatedUser, authorizeRoles("admin"),deleteProduct)
-.get(getProductDetails);
+
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { loginUser, logout, forgotPassword, resetPassword, registerUser } = require("../controllers/userController");
+const { loginUser, logout, forgotPassword, resetPassword, registerUser, getUserDetails, updatePassword, updateProfile, getAllUser, getSingleUser, updateUserRole, deleteUser } = require("../controllers/userController");
+const{authorizeRoles,isAuthenticatedUser} = require("../middleware/auth");
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -7,6 +9,12 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthenticatedUser,getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/update").put(isAuthenticatedUser,updateProfile);
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),getAllUser);
+router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser).put(isAuthenticatedUser,authorizeRoles("admin"),updateUserRole).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteUser);
+
 
 module.exports = router;
 
