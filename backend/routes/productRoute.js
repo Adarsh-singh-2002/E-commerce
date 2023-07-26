61ef6cf403e57417906ee0f5aade5f0e513a4592
Adarsh <adarshriit2017@gmail.com>
2023-07-26 11:46:36 +0530

    26072023-review Operations

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,6 +6,9 @@ const {
      updateProducts,
      deleteProduct,
      getProductDetails,
+     createProductReview,
+     getProductReviews,
+     deleteReview,
      } = require("../controllers/productController");
 
 const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
@@ -23,6 +26,11 @@ router
 .delete(isAuthenticatedUser, authorizeRoles("admin"),deleteProduct)
 
 router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser,createProductReview);
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
+
 
 module.exports = router;
 
