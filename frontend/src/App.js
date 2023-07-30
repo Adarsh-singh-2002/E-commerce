7f4169fe20feb954474ef83a244de9a5a082a058
Adarsh <adarshriit2017@gmail.com>
2023-07-30 23:21:26 +0530

    30072023-navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/layout/Header.js"
+import { BrowserRouter as Router } from "react-router-dom";
+import webFont from "webfontloader";
+import React from 'react';
+import WebFont from 'webfontloader';
 
+
+React.useEffect(() => {
+
+  WebFont.load({
+    google:{
+      families:["Roboto","Droid Sans","Chilanka"]
+    }
+  })
+
+
+}, [])
 function App() {
-  return (
-    
-  );
+  return (<Router>
+      <Header/>
+    </Router>);
+
 }
 
 export default App;
