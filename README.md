d41af21dbe6e5c2c465214edcb6751ef3758bc1e
Adarsh <adarshriit2017@gmail.com>
2023-08-01 17:52:00 +0530

    01082023-basicStructure

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A full fledged e commerce website
 
 E- commerce website for any retailer
+
+
+npm i webfontloader --force
+
+npm i react-rating-stars-component --force
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import './App.css';
-import Header from "./component/layout/Header.js"
-import { BrowserRouter as Router } from "react-router-dom";
-import webFont from "webfontloader";
+import Header from "./component/layout/Header/Header.js"
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import React from 'react';
 import WebFont from 'webfontloader';
+import Footer from "./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
 
-
+function App() {
 React.useEffect(() => {
 
   WebFont.load({
     google:{
       families:["Roboto","Droid Sans","Chilanka"]
     }
-  })
+  });
 
 
-}, [])
-function App() {
-  return (<Router>
+}, []);
+
+  return (
+    <Router>
       <Header/>
-    </Router>);
+      <Route extact path = "/" Component={Home}/>
+
+
+      <Footer/>
+    </Router>
+    );
 
 }
 
