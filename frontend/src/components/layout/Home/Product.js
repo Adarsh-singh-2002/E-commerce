dad7c6ab8300d89ce266117c417c26f1881568d2
Adarsh <adarshriit2017@gmail.com>
2023-08-08 01:40:54 +0530

    08082023_started_implementing_redux

--- frontend/src/components/layout/Home/Product.js
+++ frontend/src/components/layout/Home/Product.js
@@ -19,10 +19,10 @@ const options = {
 const Product = ({product}) =>{
     return (
         <Link className="productCard" to={Product._id}>
-            <img src={product.images[0].url} alt={product.name}/>
+            <img src={product.images[0].url} alt={product.name} />
             <p>{product.name}</p>
             <div>
-                <ReactStars {...options}/> <span>(256 reviews)</span>
+                <ReactStars {...options} /> <span>(256 reviews)</span>
             </div>
             <span>(product.price)</span>
 
