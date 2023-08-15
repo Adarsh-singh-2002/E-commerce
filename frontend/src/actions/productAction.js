c8cf17091c231604c86173c5de552635152badf2
Adarsh <adarshriit2017@gmail.com>
2023-08-15 16:47:29 +0530

    15082023-workingOnReducers

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import {ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS, CLEAR_ERRORS} from "../constants/productConstants";
+import {ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS,  CLEAR_ERRORS} from "../constants/productConstants";
 
 export const getProduct = () => async(dispatch) => {
     try{
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import store from './store';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const notify = () => toast("Wow so easy!");
 root.render(
   <Provider store={store}>
     <App />
+    {/* <ToastContainer/> */}
   </Provider>
 );
 
+// function App(){
+//   const notify = () => toast("Wow so easy!");
+
+//   return (
+//     <div>
+//       <button onClick={notify}>Notify!</button>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
